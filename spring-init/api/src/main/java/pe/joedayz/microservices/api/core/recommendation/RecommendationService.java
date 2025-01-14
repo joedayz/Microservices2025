a70fe99a7f84ad27cb475aa53676c2d4590807ca
Jose Amadeo Diaz Diaz <jose.diaz@joedayz.pe>
2025-01-13 20:26:11 -0500

    RecommendationService

---
+++ spring-init/api/src/main/java/pe/joedayz/microservices/api/core/recommendation/RecommendationService.java
@@ -0,0 +1,18 @@
+package pe.joedayz.microservices.api.core.recommendation;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author josediaz
+ **/
+public interface RecommendationService {
+
+  @GetMapping(
+      value = "/recommendation",
+      produces = "application/json"
+  )
+  List<Recommendation> getRecommendations(
+      @RequestParam(value = "productId", required = true) int productId);
+}
