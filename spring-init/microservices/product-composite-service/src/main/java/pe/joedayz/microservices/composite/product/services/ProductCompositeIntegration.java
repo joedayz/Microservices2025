aeab0e0bc226978e6049a73515a09acb232505b1
Jose Amadeo Diaz Diaz <jose.diaz@joedayz.pe>
2025-01-29 21:00:55 -0500

    ProductComposite tests

--- spring-init/microservices/product-composite-service/src/main/java/pe/joedayz/microservices/composite/product/services/ProductCompositeIntegration.java
+++ spring-init/microservices/product-composite-service/src/main/java/pe/joedayz/microservices/composite/product/services/ProductCompositeIntegration.java
@@ -51,15 +51,31 @@ public class ProductCompositeIntegration implements ProductService, Recommendati
     this.restTemplate = restTemplate;
     this.objectMapper = objectMapper;
 
-    this.productServiceUrl = "http://" + productServiceHost + ":" + productServicePort + "/product/";
-    this.recommendationServiceUrl = "http://" + recommendationServiceHost + ":" + recommendationServicePort + "/recommendation?productId=";
-    this.reviewServiceUrl = "http://" + reviewServiceHost + ":" + reviewServicePort + "/review?productId=";
+    productServiceUrl = "http://" + productServiceHost + ":" + productServicePort + "/product";
+    recommendationServiceUrl = "http://" + recommendationServiceHost + ":" + recommendationServicePort + "/recommendation";
+    reviewServiceUrl = "http://" + reviewServiceHost + ":" + reviewServicePort + "/review";
+  }
+
+  @Override
+  public Product createProduct(Product body) {
+    try {
+      String url = productServiceUrl;
+      LOG.debug("Will post a new product to URL: {}", url);
+
+      Product product = restTemplate.postForObject(url, body, Product.class);
+      LOG.debug("Created a product with id: {}", product.getProductId());
+
+      return product;
+
+    } catch (HttpClientErrorException ex) {
+      throw handleHttpClientException(ex);
+    }
   }
 
   @Override
   public Product getProduct(int productId) {
     try {
-      String url = productServiceUrl + productId;
+      String url = productServiceUrl + "/" + productId;
       LOG.debug("Call getProduct API on URL: {}", url);
 
       Product product = restTemplate.getForObject(url, Product.class);
@@ -67,26 +83,44 @@ public class ProductCompositeIntegration implements ProductService, Recommendati
 
       return product;
     }catch (HttpClientErrorException ex){
-      switch (HttpStatus.resolve(ex.getStatusCode().value())){
-        case NOT_FOUND -> throw new NotFoundException(getErrorMessage(ex));
-        case UNPROCESSABLE_ENTITY -> throw new InvalidInputException(getErrorMessage(ex));
-        default -> throw  ex;
-      }
+      throw handleHttpClientException(ex);
     }
   }
 
-  private String getErrorMessage(HttpClientErrorException ex) {
-    try{
-      return objectMapper.readValue(ex.getResponseBodyAsString(), HttpErrorInfo.class).getMessage();
-    }catch (IOException ioex){
-      return ex.getMessage();
+  @Override
+  public void deleteProduct(int productId) {
+    try {
+      String url = productServiceUrl + "/" + productId;
+      LOG.debug("Will call the deleteProduct API on URL: {}", url);
+
+      restTemplate.delete(url);
+
+    } catch (HttpClientErrorException ex) {
+      throw handleHttpClientException(ex);
+    }
+  }
+
+
+  @Override
+  public Recommendation createRecommendation(Recommendation body) {
+    try {
+      String url = recommendationServiceUrl;
+      LOG.debug("Will post a new recommendation to URL: {}", url);
+
+      Recommendation recommendation = restTemplate.postForObject(url, body, Recommendation.class);
+      LOG.debug("Created a recommendation with id: {}", recommendation.getProductId());
+
+      return recommendation;
+
+    } catch (HttpClientErrorException ex) {
+      throw handleHttpClientException(ex);
     }
   }
 
   @Override
   public List<Recommendation> getRecommendations(int productId) {
     try{
-      String url = recommendationServiceUrl + productId;
+      String url = recommendationServiceUrl + "?productId=" + productId;
       LOG.debug("Call getRecommendations API on URL: {}", url);
 
       List<Recommendation> recommendations = restTemplate.exchange(url, GET, null,
@@ -104,9 +138,38 @@ public class ProductCompositeIntegration implements ProductService, Recommendati
   }
 
   @Override
+  public void deleteRecommendations(int productId) {
+    try {
+      String url = recommendationServiceUrl + "?productId=" + productId;
+      LOG.debug("Will call the deleteRecommendations API on URL: {}", url);
+
+      restTemplate.delete(url);
+
+    } catch (HttpClientErrorException ex) {
+      throw handleHttpClientException(ex);
+    }
+  }
+
+  @Override
+  public Review createReview(Review body) {
+    try {
+      String url = reviewServiceUrl;
+      LOG.debug("Will post a new review to URL: {}", url);
+
+      Review review = restTemplate.postForObject(url, body, Review.class);
+      LOG.debug("Created a review with id: {}", review.getProductId());
+
+      return review;
+
+    } catch (HttpClientErrorException ex) {
+      throw handleHttpClientException(ex);
+    }
+  }
+
+  @Override
   public List<Review> getReviews(int productId) {
     try{
-      String url = reviewServiceUrl + productId;
+      String url = reviewServiceUrl + "?productId=" + productId;
       LOG.debug("Call getReviews API on URL: {}", url);
 
       List<Review> reviews = restTemplate.exchange(url, GET, null,
@@ -119,6 +182,43 @@ public class ProductCompositeIntegration implements ProductService, Recommendati
     }catch (Exception ex){
       LOG.warn("Got an exception while getting reviews: {}", ex.getMessage());
       return new ArrayList<>();
+    }
+  }
+
+  @Override
+  public void deleteReviews(int productId) {
+    try {
+      String url = reviewServiceUrl + "?productId=" + productId;
+      LOG.debug("Will call the deleteReviews API on URL: {}", url);
+
+      restTemplate.delete(url);
+
+    } catch (HttpClientErrorException ex) {
+      throw handleHttpClientException(ex);
+    }
+  }
+
+  private RuntimeException handleHttpClientException(HttpClientErrorException ex) {
+    switch (HttpStatus.resolve(ex.getStatusCode().value())) {
+
+      case NOT_FOUND:
+        return new NotFoundException(getErrorMessage(ex));
+
+      case UNPROCESSABLE_ENTITY:
+        return new InvalidInputException(getErrorMessage(ex));
+
+      default:
+        LOG.warn("Got an unexpected HTTP error: {}, will rethrow it", ex.getStatusCode());
+        LOG.warn("Error body: {}", ex.getResponseBodyAsString());
+        return ex;
+    }
+  }
+
+  private String getErrorMessage(HttpClientErrorException ex) {
+    try {
+      return objectMapper.readValue(ex.getResponseBodyAsString(), HttpErrorInfo.class).getMessage();
+    } catch (IOException ioex) {
+      return ex.getMessage();
     }
   }
 }
--- spring-init/microservices/product-composite-service/src/main/java/pe/joedayz/microservices/composite/product/services/ProductCompositeServiceImpl.java
+++ spring-init/microservices/product-composite-service/src/main/java/pe/joedayz/microservices/composite/product/services/ProductCompositeServiceImpl.java
@@ -2,6 +2,9 @@ package pe.joedayz.microservices.composite.product.services;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import pe.joedayz.microservices.api.composite.ProductAggregate;
 import pe.joedayz.microservices.api.composite.ProductCompositeService;
@@ -20,13 +23,52 @@ import pe.joedayz.microservices.util.http.ServiceUtil;
 @RestController
 public class ProductCompositeServiceImpl implements ProductCompositeService {
 
-  private final ServiceUtil serviceUtil;
-  private final ProductCompositeIntegration integration;
+  private static final Logger LOG = LoggerFactory.getLogger(ProductCompositeServiceImpl.class);
 
-  public ProductCompositeServiceImpl(ServiceUtil serviceUtil,
-      ProductCompositeIntegration integration) {
+  private final ServiceUtil serviceUtil;
+  private ProductCompositeIntegration integration;
+
+  @Autowired
+  public ProductCompositeServiceImpl(
+      ServiceUtil serviceUtil, ProductCompositeIntegration integration) {
+
     this.serviceUtil = serviceUtil;
     this.integration = integration;
+  }
+
+  @Override
+  public void createProduct(ProductAggregate body) {
+    try {
+
+      LOG.debug("createCompositeProduct: creates a new composite entity for productId: {}",
+          body.getProductId());
+
+      Product product = new Product(body.getProductId(), body.getName(), body.getWeight(), null);
+      integration.createProduct(product);
+
+      if (body.getRecommendations() != null) {
+        body.getRecommendations().forEach(r -> {
+          Recommendation recommendation = new Recommendation(body.getProductId(),
+              r.getRecommendationId(), r.getAuthor(), r.getRate(), r.getContent(), null);
+          integration.createRecommendation(recommendation);
+        });
+      }
+
+      if (body.getReviews() != null) {
+        body.getReviews().forEach(r -> {
+          Review review = new Review(body.getProductId(), r.getReviewId(), r.getAuthor(),
+              r.getSubject(), r.getContent(), null);
+          integration.createReview(review);
+        });
+      }
+
+      LOG.debug("createCompositeProduct: composite entities created for productId: {}",
+          body.getProductId());
+
+    } catch (RuntimeException re) {
+      LOG.warn("createCompositeProduct failed", re);
+      throw re;
+    }
   }
 
   @Override
@@ -40,32 +82,55 @@ public class ProductCompositeServiceImpl implements ProductCompositeService {
     List<Recommendation> recommendations = integration.getRecommendations(productId);
     List<Review> reviews = integration.getReviews(productId);
 
-    return createProductAggregate(product, recommendations, reviews, serviceUtil.getServiceAddress());
+    return createProductAggregate(product, recommendations, reviews,
+        serviceUtil.getServiceAddress());
   }
 
-  private ProductAggregate createProductAggregate(Product product, List<Recommendation> recommendations,
+  @Override
+  public void deleteProduct(int productId) {
+    LOG.debug("deleteCompositeProduct: Deletes a product aggregate for productId: {}", productId);
+
+    integration.deleteProduct(productId);
+
+    integration.deleteRecommendations(productId);
+
+    integration.deleteReviews(productId);
+
+    LOG.debug("deleteCompositeProduct: aggregate entities deleted for productId: {}", productId);
+  }
+
+  private ProductAggregate createProductAggregate(Product product,
+      List<Recommendation> recommendations,
       List<Review> reviews, String serviceAddress) {
 
-    int productId =  product.getProductId();
+    int productId = product.getProductId();
     String name = product.getName();
     int weight = product.getWeight();
 
     List<RecommendationSummary> recommendationSummaries =
-        (recommendations==null)? null: recommendations.stream().map(r -> new RecommendationSummary(r.getRecommendationId(), r.getAuthor(), r.getRate()))
+        (recommendations == null) ? null : recommendations.stream().map(
+                r -> new RecommendationSummary(r.getRecommendationId(), r.getAuthor(), r.getRate(),
+                    r.getContent()))
             .collect(Collectors.toList());
 
-    List<ReviewSummary> reviewSummaries =
-        (reviews == null) ? null : reviews.stream()
-            .map(r -> new ReviewSummary(r.getReviewId(), r.getAuthor(), r.getSubject()))
+    List<ReviewSummary> reviewSummaries = (reviews == null) ? null :
+        reviews.stream()
+            .map(r -> new ReviewSummary(r.getReviewId(), r.getAuthor(), r.getSubject(),
+                r.getContent()))
             .collect(Collectors.toList());
 
     String productAddress = product.getServiceAddress();
-    String reviewAddress = (reviews!=null && reviews.size()>0) ? reviews.get(0).getServiceAddress() : "";
-    String recommendationAddress = (recommendations!=null && recommendations.size()>0) ?recommendations.get(0).getServiceAddress() : "";
+    String reviewAddress =
+        (reviews != null && reviews.size() > 0) ? reviews.get(0).getServiceAddress() : "";
+    String recommendationAddress =
+        (recommendations != null && recommendations.size() > 0) ? recommendations.get(0)
+            .getServiceAddress() : "";
 
-    ServiceAddresses serviceAddresses = new ServiceAddresses(serviceAddress, productAddress, reviewAddress, recommendationAddress);
+    ServiceAddresses serviceAddresses = new ServiceAddresses(serviceAddress, productAddress,
+        reviewAddress, recommendationAddress);
 
-    return new ProductAggregate(productId, name, weight, recommendationSummaries, reviewSummaries, serviceAddresses);
+    return new ProductAggregate(productId, name, weight, recommendationSummaries, reviewSummaries,
+        serviceAddresses);
 
   }
 }
