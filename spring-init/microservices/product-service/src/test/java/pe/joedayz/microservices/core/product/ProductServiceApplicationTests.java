cc0857710b59b8b85008aaaa9ad6f0809c857f2c
Jose Amadeo Diaz Diaz <jose.diaz@joedayz.pe>
2025-03-03 20:40:36 -0500

    Product service con mensajeria listo

--- spring-init/microservices/product-service/src/test/java/pe/joedayz/microservices/core/product/ProductServiceApplicationTests.java
+++ spring-init/microservices/product-service/src/test/java/pe/joedayz/microservices/core/product/ProductServiceApplicationTests.java
@@ -1,21 +1,29 @@
 package pe.joedayz.microservices.core.product;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static reactor.core.publisher.Mono.just;
+import static pe.joedayz.microservices.api.event.Event.Type.CREATE;
+import static pe.joedayz.microservices.api.event.Event.Type.DELETE;
+
+import java.util.function.Consumer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import pe.joedayz.microservices.api.core.product.Product;
+import pe.joedayz.microservices.api.event.Event;
+import pe.joedayz.microservices.api.exceptions.InvalidInputException;
 import pe.joedayz.microservices.core.product.persistence.ProductRepository;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
@@ -25,17 +33,31 @@ class ProductServiceApplicationTests extends MongoDbTestBase {
 
 	@Autowired private ProductRepository repository;
 
+	@Autowired
+	@Qualifier("messageProcessor")
+	private Consumer<Event<Integer, Product>> messageProcessor;
+
 	@BeforeEach
 	void setupDb() {
-		repository.deleteAll();
+		repository.deleteAll().block();
 	}
+
 
 	@Test
 	void getProductById() {
+
 		int productId = 1;
-		postAndVerifyProduct(productId, OK);
-		assertTrue(repository.findByProductId(productId).isPresent());
-		getAndVerifyProduct(productId, OK).jsonPath("$.productId").isEqualTo(productId);
+
+		assertNull(repository.findByProductId(productId).block());
+		assertEquals(0, (long)repository.count().block());
+
+		sendCreateProductEvent(productId);
+
+		assertNotNull(repository.findByProductId(productId).block());
+		assertEquals(1, (long)repository.count().block());
+
+		getAndVerifyProduct(productId, OK)
+				.jsonPath("$.productId").isEqualTo(productId);
 	}
 
 
@@ -44,27 +66,30 @@ class ProductServiceApplicationTests extends MongoDbTestBase {
 
 		int productId = 1;
 
-		postAndVerifyProduct(productId, OK);
+		assertNull(repository.findByProductId(productId).block());
 
-		assertTrue(repository.findByProductId(productId).isPresent());
+		sendCreateProductEvent(productId);
 
-		postAndVerifyProduct(productId, UNPROCESSABLE_ENTITY)
-				.jsonPath("$.path").isEqualTo("/product")
-				.jsonPath("$.message").isEqualTo("Duplicate key, Product Id: " + productId);
+		assertNotNull(repository.findByProductId(productId).block());
+
+		InvalidInputException thrown = assertThrows(
+				InvalidInputException.class,
+				() -> sendCreateProductEvent(productId),
+				"Expected a InvalidInputException here!");
+		assertEquals("Duplicate key, Product Id: " + productId, thrown.getMessage());
 	}
-
 	@Test
 	void deleteProduct() {
 
 		int productId = 1;
 
-		postAndVerifyProduct(productId, OK);
-		assertTrue(repository.findByProductId(productId).isPresent());
+		sendCreateProductEvent(productId);
+		assertNotNull(repository.findByProductId(productId).block());
 
-		deleteAndVerifyProduct(productId, OK);
-		assertFalse(repository.findByProductId(productId).isPresent());
+		sendDeleteProductEvent(productId);
+		assertNull(repository.findByProductId(productId).block());
 
-		deleteAndVerifyProduct(productId, OK);
+		sendDeleteProductEvent(productId);
 	}
 
 	@Test
@@ -108,25 +133,15 @@ class ProductServiceApplicationTests extends MongoDbTestBase {
 				.expectBody();
 	}
 
-	private WebTestClient.BodyContentSpec postAndVerifyProduct(int productId, HttpStatus expectedStatus) {
+	private void sendCreateProductEvent(int productId) {
 		Product product = new Product(productId, "Name " + productId, productId, "SA");
-		return client.post()
-				.uri("/product")
-				.body(just(product), Product.class)
-				.accept(APPLICATION_JSON)
-				.exchange()
-				.expectStatus().isEqualTo(expectedStatus)
-				.expectHeader().contentType(APPLICATION_JSON)
-				.expectBody();
+		Event<Integer, Product> event = new Event(CREATE, productId, product);
+		messageProcessor.accept(event);
 	}
 
-	private WebTestClient.BodyContentSpec deleteAndVerifyProduct(int productId, HttpStatus expectedStatus) {
-		return client.delete()
-				.uri("/product/" + productId)
-				.accept(APPLICATION_JSON)
-				.exchange()
-				.expectStatus().isEqualTo(expectedStatus)
-				.expectBody();
+	private void sendDeleteProductEvent(int productId) {
+		Event<Integer, Product> event = new Event(DELETE, productId, null);
+		messageProcessor.accept(event);
 	}
 
 }
